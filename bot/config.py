0c90b0998904c9c3c40907b437048088a5642ae4
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2022-10-16 15:01:59 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,7 +9,7 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1430593323
     OWNER = config("OWNER")
-    ffmpegcode = ["-preset fast -s 1280x720 -x265-params -metadata 'title=Encoded By TGVid-Comp (https://t.me/animecolony)' -pix_fmt yuv420p -crf 25 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
+    ffmpegcode = ["-preset fast -s 1280x720 -metadata 'title=Encoded By TGVid-Comp (https://t.me/animecolony)' -pix_fmt yuv420p -crf 25 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/file/de709efff463ace84b278.jpg")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
